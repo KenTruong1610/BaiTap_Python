fef9591e767ca622f1a891ad92b6e4542891c373
Ken <thanhtruongcong.15092001@gmail.com>
2024-10-14 01:49:54 +0700

    first

--- BT10.py
+++ BT10.py
@@ -1,8 +1,8 @@
 """
 Bài 10: Nhập tên và số tiền của nhân viên đi làm theo tháng, biết:
-"""
 Nếu đi làm dưới 2 tháng trong năm: 500000 VND
 Nếu đi làm trên 2 tháng trong năm: Số tháng x 250000VND
+"""
 a = input ("Nhập tên của nhân viên: ")
 b = int (input("Nhập số tháng đi làm: "))
 if b <= 2 :
